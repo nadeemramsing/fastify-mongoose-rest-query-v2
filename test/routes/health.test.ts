262f97f79d6c845dd43ab76904eca7d4865e04fe
nadeemramsing-md <nadeem.r@marideal.mu>
2025-02-22 23:10:21 +0400

    PostgreSqlContainer added

--- test/routes/health.test.ts
+++ test/routes/health.test.ts
@@ -1,26 +1,26 @@
-import { buildApp } from "../setup/app";
-import { FastifyInstance } from "fastify";
-import { describe, it, before, after } from "node:test";
-import assert from "assert";
+import { buildApp } from '../setup/app'
+import { FastifyInstance } from 'fastify'
+import { describe, it, before, after } from 'node:test'
+import assert from 'assert'
 
-describe("GET /health HTTP", () => {
-  let app: FastifyInstance;
+describe('GET /health HTTP', () => {
+  let app: FastifyInstance
 
   before(async () => {
-    app = await buildApp({ logger: false });
-  });
+    app = await buildApp({ logger: false })
+  })
 
   after(async () => {
-    await app.close();
-  });
+    await app.close()
+  })
 
-  it("GET /health returns status 200", async () => {
+  it('GET /health returns status 200', async () => {
     const response = await app.inject({
-      method: "GET",
-      url: "/api/health",
-    });
+      method: 'GET',
+      url: '/api/health',
+    })
 
-    assert.strictEqual(response.statusCode, 200);
-    assert.strictEqual(JSON.parse(response.payload).status, "ok");
-  });
-});
+    assert.strictEqual(response.statusCode, 200)
+    assert.strictEqual(JSON.parse(response.payload).status, 'ok')
+  })
+})
--- test/setup/app.ts
+++ test/setup/app.ts
@@ -1,25 +1,32 @@
-import Fastify, { FastifyInstance } from "fastify";
-import autoload from "@fastify/autoload";
-import { join } from "path";
-import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import Fastify, { FastifyInstance } from 'fastify'
+import autoload from '@fastify/autoload'
+import { join } from 'path'
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
+import { PostgreSqlContainer } from '@testcontainers/postgresql'
 
 const defaultOptions = {
   logger: true,
   ignoreTrailingSlash: true,
-};
+}
 
 async function buildApp(options: Partial<typeof defaultOptions> = {}) {
+  const container = await new PostgreSqlContainer().start()
+
   const app: FastifyInstance = Fastify({
     ...defaultOptions,
     ...options,
-  }).withTypeProvider<TypeBoxTypeProvider>();
+  }).withTypeProvider<TypeBoxTypeProvider>()
 
   app.register(autoload, {
-    dir: join(__dirname, "routes"),
-    options: { prefix: "/api" },
-  });
+    dir: join(__dirname, 'routes'),
+    options: { prefix: '/api' },
+  })
 
-  return app;
+  console.log(container.getConnectionUri())
+
+  container.stop()
+
+  return app
 }
 
-export { buildApp };
+export { buildApp }
