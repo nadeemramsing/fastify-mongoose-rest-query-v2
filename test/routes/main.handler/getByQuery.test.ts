1fbfcf6f9032bd926977a31cfeec2fddb780bb98
nadeemramsing-md <nadeem.ramsing@gmail.com>
2025-02-24 21:44:26 +0400

    getByQuery test improved

--- test/routes/main.handler/getByQuery.test.ts
+++ test/routes/main.handler/getByQuery.test.ts
@@ -1,9 +1,10 @@
 import { app } from '@test/setup/setup'
 import { resources } from '@test/setup/fixtures/resources'
 import { getDocsInJSON } from '@test/setup/fixtures/get-docs-in-json.method'
+import { filter } from 'lodash/fp'
 
-describe('/', () => {
-  it('GET /health returns status 200', async () => {
+describe('/ GET (getByQuery)', () => {
+  it('should return all resources with no filter', async () => {
     const response = await app.inject({
       method: 'GET',
       url: '/secure/mrq/resources',
@@ -13,6 +14,25 @@ describe('/', () => {
     })
 
     const resourcesExpected = getDocsInJSON(resources)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toMatchObject(resourcesExpected)
+  })
+
+  it('should return resources with 1 filter name=Nadeem', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: '/secure/mrq/resources',
+      query: {
+        select: 'all',
+        filter: JSON.stringify({ name: 'Nadeem' }),
+      },
+    })
+
+    const resourcesExpected = filter(
+      { name: 'Nadeem' },
+      getDocsInJSON(resources)
+    )
 
     expect(response.statusCode).toBe(200)
     expect(response.json()).toMatchObject(resourcesExpected)
