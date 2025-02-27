58603c03687519d5f78f176bada86b4217135383
nadeemramsing-md <nadeem.ramsing@gmail.com>
2025-02-27 18:49:20 +0400

    Tests in getById added

--- test/routes/main.param.handler/getById.test.ts
+++ test/routes/main.param.handler/getById.test.ts
@@ -2,7 +2,7 @@ import { getDocsInJSON } from '@test/setup/fixtures/get-docs-in-json.method'
 import { resources } from '@test/setup/fixtures/resources'
 import { app } from '@test/setup/setup'
 import { ObjectId } from 'bson'
-import { find } from 'lodash/fp'
+import { find, pick, pipe } from 'lodash/fp'
 
 const resourceId = ObjectId.createFromTime(1)
 const resourceIdZak = ObjectId.createFromTime(3)
@@ -22,5 +22,233 @@ describe.sequential('/:id GET (getById)', () => {
     expect(response.statusCode).toBe(200)
     expect(response.json()).toEqual(resourceExpected)
   })
+
+  it('should return resource with specified id and 1 filter name=Nadeem', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: `/secure/admin/mrq/resources/${resourceId}`,
+      query: {
+        select: 'all',
+        filter: JSON.stringify({ name: 'Nadeem' }),
+      },
+    })
+
+    const resourceExpected = find({ name: 'Nadeem' }, getDocsInJSON(resources))
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toEqual(resourceExpected)
+  })
+
+  it('should return resource with specified id and 2 filters name=~mira&age>=10', async () => {
+    const resourceId = ObjectId.createFromTime(2)
+
+    const response = await app.inject({
+      method: 'GET',
+      url: `/secure/admin/mrq/resources/${resourceId}`,
+      query: {
+        select: 'all',
+        filter: JSON.stringify({ name: { $regex: 'mira' }, age: { $gte: 10 } }),
+      },
+    })
+
+    const resourceExpected = find(
+      (doc: any) => RegExp('mira', 'i').test(doc.name) && doc.age >= 10,
+      getDocsInJSON(resources)
+    )
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toEqual(resourceExpected)
+  })
+
+  it('should return resource with specified id and 1 filter addresses.street=street3', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: `/secure/admin/mrq/resources/${resourceIdZak}`,
+      query: {
+        select: 'all',
+        filter: JSON.stringify({ 'addresses.street': 'street3' }),
+      },
+    })
+
+    const resourceExpected = find(
+      (doc: any) =>
+        doc.addresses.find((addr: any) => addr.street === 'street3'),
+      getDocsInJSON(resources)
+    )
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toEqual(resourceExpected)
+  })
+
+  it('should return resource with specified id and 1 filter noField=null (i.e. all)', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: `/secure/admin/mrq/resources/${resourceId}`,
+      query: {
+        select: 'all',
+        filter: JSON.stringify({ noField: null }),
+      },
+    })
+
+    const resourceExpected = find(
+      (doc: any) => !doc.noField,
+      getDocsInJSON(resources)
+    )
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toEqual(resourceExpected)
+  })
   //#endregion filter
+
+  it('should return resource with specified id and selected fields name,age', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: `/secure/admin/mrq/resources/${resourceId}`,
+      query: {
+        select: 'name,age',
+      },
+    })
+
+    const resourceExpected = pipe(
+      find({ _id: resourceId.toString() }),
+      pick(['_id', 'name', 'age'])
+    )(getDocsInJSON(resources))
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toEqual(resourceExpected)
+  })
+
+  it('should return resource with specified id and selected fields name, age', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: `/secure/admin/mrq/resources/${resourceId}`,
+      query: {
+        select: 'name, age',
+      },
+    })
+
+    const resourceExpected = pipe(
+      find({ _id: resourceId.toString() }),
+      pick(['_id', 'name', 'age'])
+    )(getDocsInJSON(resources))
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toEqual(resourceExpected)
+  })
+
+  it('should return resource with specified id and selected fields name, age, addresses and selected static method firstCity ', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: `/secure/admin/mrq/resources/${resourceId}`,
+      query: {
+        select: 'name, age, addresses, firstCity',
+      },
+    })
+
+    const resourceExpected: any = pipe(
+      find({ _id: resourceId.toString() }),
+      pick(['_id', 'name', 'age', 'addresses'])
+    )(getDocsInJSON(resources))
+
+    resourceExpected['firstCity'] = resourceExpected.addresses.find(
+      (address: any) => address.flags?.is_a_city
+    )?.city
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toEqual(resourceExpected)
+  })
+
+  it('should return resource with specified id, name=Zakariyya and populated path father (using string)', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: `/secure/admin/mrq/resources/${resourceIdZak}`,
+      query: {
+        select: 'all',
+        populate: 'father',
+        filter: JSON.stringify({ name: 'Zakariyya' }),
+      },
+    })
+
+    const resourcesJSON = getDocsInJSON(resources)
+
+    const resourceExpected: any = find({ name: 'Zakariyya' }, resourcesJSON)
+
+    const father = find({ _id: resourceExpected.father }, resourcesJSON)
+
+    resourceExpected.father = father
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toMatchObject(resourceExpected)
+  })
+
+  it('should return resource with specified id, name=Zakariyya and populated path father (using object with path, match, select)', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: `/secure/admin/mrq/resources/${resourceIdZak}`,
+      query: {
+        select: 'all',
+        populate: JSON.stringify({
+          path: 'father',
+          match: { age: { $gte: 28 } },
+        }),
+        filter: JSON.stringify({ name: 'Zakariyya' }),
+      },
+    })
+
+    const resourcesJSON = getDocsInJSON(resources)
+
+    const resourceExpected: any = find({ name: 'Zakariyya' }, resourcesJSON)
+
+    const father = pipe(
+      find({ _id: resourceExpected.father }),
+      pick('name')
+    )(resourcesJSON)
+
+    resourceExpected.father = father
+
+    expect(response.json()).toMatchObject(resourceExpected)
+  })
+
+  it('should return resource with name=Zakariyya and populated paths father and mother (using array of objects with path, match, select)', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: `/secure/admin/mrq/resources/${resourceIdZak}`,
+      query: {
+        select: 'all',
+        populate: JSON.stringify([
+          {
+            path: 'father',
+            match: { age: { $gte: 28 } },
+            select: 'name',
+          },
+          {
+            path: 'mother',
+            match: { age: { $gte: 24 } },
+            select: 'name',
+          },
+        ]),
+        filter: JSON.stringify({ name: 'Zakariyya' }),
+      },
+    })
+
+    const resourcesJSON = getDocsInJSON(resources)
+
+    const resourceExpected: any = find({ name: 'Zakariyya' }, resourcesJSON)
+
+    const father = pipe(
+      find({ _id: resourceExpected.father }),
+      pick('name')
+    )(resourcesJSON)
+
+    resourceExpected.father = father
+
+    const mother = pipe(
+      find({ _id: resourceExpected.mother }),
+      pick('name')
+    )(resourcesJSON)
+
+    resourceExpected.mother = mother
+
+    expect(response.json()).toMatchObject(resourceExpected)
+  })
 })
