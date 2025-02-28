7dc483fb6908ac8eac5950acda51e1de996a1957
nadeemramsing-md <nadeem.ramsing@gmail.com>
2025-02-28 20:23:47 +0400

    More tests added in getByQuery.sub

--- test/routes/main.param.subarray.handler/getByQuery.sub.test.ts
+++ test/routes/main.param.subarray.handler/getByQuery.sub.test.ts
@@ -2,7 +2,7 @@ import { getDocsInJSON } from '@test/setup/fixtures/get-docs-in-json.method'
 import { resources } from '@test/setup/fixtures/resources'
 import { app } from '@test/setup/setup'
 import { ObjectId } from 'bson'
-import { find } from 'lodash/fp'
+import { filter, find } from 'lodash/fp'
 
 const resourceId = ObjectId.createFromTime(1)
 
@@ -23,5 +23,50 @@ describe.sequential('/:id/subarray GET (getByQuery.sub)', () => {
 
     expect(response.statusCode).toBe(200)
     expect(response.json()).toEqual(resource.addresses)
+  })
+
+  it('should return all addresses of resource with specified id and city=Beau Bassin', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: `/secure/admin/mrq/resources/${resourceId}/addresses`,
+      query: {
+        select: 'all',
+        filter: JSON.stringify({ city: 'Beau Bassin' }),
+      },
+    })
+
+    const resource: any = find(
+      { _id: resourceId.toString() },
+      getDocsInJSON(resources)
+    )
+
+    const addresses = filter({ city: 'Beau Bassin' }, resource.addresses)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toMatchObject(addresses)
+  })
+
+  it('should return all addresses of resource with specified id and street ending with "A"', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: `/secure/admin/mrq/resources/${resourceId}/addresses`,
+      query: {
+        select: 'all',
+        filter: JSON.stringify({ street: { $regex: 'A$' } }),
+      },
+    })
+
+    const resource: any = find(
+      { _id: resourceId.toString() },
+      getDocsInJSON(resources)
+    )
+
+    const addresses = filter(
+      (doc) => doc.street?.endsWith?.('A'),
+      resource.addresses
+    )
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toMatchObject(addresses)
   })
 })
