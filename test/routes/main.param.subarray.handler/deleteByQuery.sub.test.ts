e457f08b1f979615fcecbd31f9ecf3dabd334f16
nadeemramsing-md <nadeem.ramsing@gmail.com>
2025-03-08 13:31:44 +0400

    Test added in deleteByQuery.sub

--- test/routes/main.param.subarray.handler/deleteByQuery.sub.test.ts
+++ test/routes/main.param.subarray.handler/deleteByQuery.sub.test.ts
@@ -1,5 +1,9 @@
+import { getDocsInJSON } from '@test/setup/fixtures/get-docs-in-json.method'
+import { resources } from '@test/setup/fixtures/resources'
+import { mongoInit } from '@test/setup/init/mongo.init'
 import { app } from '@test/setup/setup'
 import { ObjectId } from 'bson'
+import { filter, find } from 'lodash/fp'
 
 const resourceId = ObjectId.createFromTime(1)
 
@@ -13,9 +17,7 @@ describe.sequential('/:id/subarray DELETE (deleteByQuery)', () => {
       },
     })
 
-    const $in = resGet
-      .json()
-      .map((doc: any) => doc._id)
+    const $in = resGet.json().map((doc: any) => doc._id)
 
     const resDel = await app.inject({
       method: 'DELETE',
@@ -27,5 +29,30 @@ describe.sequential('/:id/subarray DELETE (deleteByQuery)', () => {
 
     expect(resDel.statusCode).toBe(200)
     expect(resDel.json()).toMatchObject([])
+  })
+
+  it('should delete and return deletedCount of all addresses of the resource with specified id and street ending with "A"', async () => {
+    await mongoInit()
+
+    const response = await app.inject({
+      method: 'DELETE',
+      url: `/secure/admin/mrq/resources/${resourceId}/addresses`,
+      query: {
+        filter: JSON.stringify({ street: { $regex: 'A$' } }),
+      },
+    })
+
+    const resource: any = find(
+      { _id: resourceId.toString() },
+      getDocsInJSON(resources)
+    )
+
+    const addressesNotDeleted = filter(
+      (doc) => !doc.street?.endsWith?.('A'),
+      resource.addresses
+    )
+
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toMatchObject(addressesNotDeleted)
   })
 })
